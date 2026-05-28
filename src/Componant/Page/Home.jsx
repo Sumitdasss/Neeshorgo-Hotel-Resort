ea2215e5c51e8ab909c28bfe3ff62481f4e7bff6
Sumitdasss <sumitdas46123@gmail.com>
2026-05-28 23:40:53 +0600

    add

--- src/Componant/Page/Home.jsx
+++ src/Componant/Page/Home.jsx
@@ -1,6 +1,6 @@
 import Bannerfour from "/src/Componant/Layout/Bannerfour"
 import Bannerthreee from "/src/Componant/Layout/Bannerthreee"
-import Bannertwo from "/src/Componant/Layout/bannertwo"
+import Bannertwo from "/src/Componant/Layout/Bannertwo"
 import Bannner from "/src/Componant/Layout/Bannner"
 import Sliderx from "/src/Componant/Layout/Slider"
 
