3a2b3d72b95bd0d8c1cc4d8cc3721fa1f2dc76cf
Sumitdasss <sumitdas46123@gmail.com>
2026-05-28 23:38:43 +0600

    add

--- src/Componant/Page/Home.jsx
+++ src/Componant/Page/Home.jsx
@@ -1,8 +1,8 @@
-import Bannerfour from "../Layout/Bannerfour"
-import Bannerthreee from "../Layout/Bannerthreee"
-import Bannertwo from "../Layout/bannertwo"
-import Bannner from "../Layout/Bannner"
-import Sliderx from "../Layout/Slider"
+import Bannerfour from "/src/Componant/Layout/Bannerfour"
+import Bannerthreee from "/src/Componant/Layout/Bannerthreee"
+import Bannertwo from "/src/Componant/Layout/bannertwo"
+import Bannner from "/src/Componant/Layout/Bannner"
+import Sliderx from "/src/Componant/Layout/Slider"
 
 
 const Home = () => {
