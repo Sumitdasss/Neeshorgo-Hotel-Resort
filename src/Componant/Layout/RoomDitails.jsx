2f61103b2ade1f9d690b890b0987c343d5395ea2
Sumitdasss <sumitdas46123@gmail.com>
2026-05-30 18:01:05 +0600

    add

--- src/Componant/Layout/RoomDitails.jsx
+++ src/Componant/Layout/RoomDitails.jsx
@@ -32,9 +32,12 @@ const settings = {
   dots: true,
   infinite: true,
   speed: 500,
-  slidesToShow: 3,
-  slidesToScroll: 1, // এটি ১ রাখুন
+  slidesToShow: 1,
+  slidesToScroll: 1,
   autoplay: true,
+  autoplaySpeed: 3000,
+  pauseOnHover: false,
+  accessibility: false,
   responsive: [
     {
       breakpoint: 1024,
